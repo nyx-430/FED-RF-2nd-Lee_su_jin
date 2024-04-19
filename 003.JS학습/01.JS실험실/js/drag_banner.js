ae98da76487eb35b8bbee390e44cca6ce9a871b9
Sujin <9_ight@naver.com>
2024-04-19 17:20:47 +0900

    0419/17:20

--- 003.JS학습/01.JS실험실/js/drag_banner.js
+++ 003.JS학습/01.JS실험실/js/drag_banner.js
@@ -1,16 +1,9 @@
 // JS실험실 : 07.드래그배너 JS - drag_banner.js
 
 // [1] 모듈 함수 불러오기 //////////////////////
-// 슬라이드 기능 함수 불러오기
-import setSlide from "./slide_fn.js";
-
-// 드래그 기능 함수 불러오기
-import setDrag from "./slide_drag.js";
-
+// 드래그 슬라이드 기능 함수 불러오기
+import setSlide from "./drag_slide.js";
 
 // [2] 기능 구현 파트 //////////////////////
-// 슬라이드 기능 함수 호출하기
+// 드래그 슬라이드 기능 함수 호출하기
 setSlide('banbx');
-
-// 2. 드래그 기능 함수 호출하기
-setDrag('slide');
