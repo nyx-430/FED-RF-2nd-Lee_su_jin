3c960300010e01c807467ecc752209c9175f21c7
Sujin <9_ight@naver.com>
2024-04-30 13:20:53 +0900

    0430/13:20

---
+++ 004.React학습/01.react-cdn/js/05.Props.jsx
@@ -0,0 +1,104 @@
+// 05.리액트 Props
+
+// 나의 함수 불러오기
+import mFn from "./my_function";
+
+// 자동차 정보
+import { carInfo } from "./car_data";
+
+console.log(carInfo);
+
+/********************************************************** 
+    [ 리액트 Props ]
+    1. 리액트 구성요소에 전달되는 인수다!(전달변수)
+    2. HTML 속성을 통해서 구성요소에 전달된다
+    3. props는 속성이다.
+    4. JS 함수에 셋팅되는 전달변수와 HTML속성과 동일함
+    5. 컴포넌트로 보내기 위해서는 HTML속성과 동일한 구문 사용
+**********************************************************/
+
+/// 자기 차를 소개하는 컴포넌트 1 ///
+// function IntroCar(props){
+function IntroCar({ brand, modelNum }) {
+  // 전달속성값 : brand - 자동차 분류명 / modelNum - 배열 순번
+  // props는 컴포넌트 호출시 속성값을 객체로 전달하는 것을 받아주는 변수다
+  // 변수명은 자유롭게 쓸 수 있음!
+  // 사용법: props.속성명
+  // 주의: 속성명은 컴포넌트를 호출하는 곳에서 정해진 이름을 똑같이 사용해야 한다
+  // 명시적으로 변수명을 사용하려면 구조분해 할당형식을 사용하면 된다
+  // -> {변수명,변수명,...}
+  // 구조분해 할당으로 받을 경우의 장점은 특정 속석명을 지정하는 효과가 있고,
+  // 몇 개의 어떤 속성을 받는지 개발자가 명시적으로 알 수 있다
+
+  // 공통 정보 변수에 담기
+  // -> carInfo 하위 [자동차명] 하위 [순번] -> 최종 데이터 선택
+  let setInfo = carInfo[brand][modelNum];
+
+  return (
+    <React.Fragment>
+      <h2>나의 차는 {brand}입니다!</h2>
+      {/* 추가 질문 컴포넌트 호출 */}
+      <AskMoreInfo
+        model={setInfo.model}
+        color={setInfo.color}
+        opt={setInfo.opt}
+      />
+    </React.Fragment>
+  );
+} ///////// IntroCar 컴포넌트 /////////
+
+/* 
+    [ 데이터 매칭하기 : 속성 셋팅을 위한 기초 ]
+    1. 자동차 분류명 : carInfo 데이터변수 객체의 속성명
+    2. 상세모델 배열번호 : 선택객체 속성값 하위 배열번호
+    -> 위의 두 가지 값을 전달하면 상세정보를 보낼 수 있다
+*/
+
+/// 추가 질문으로 자동차 정보를 자세히 기술하는 컴포넌트 ///
+function AskMoreInfo({ model, color, opt }) {
+  return (
+    <React.Fragment>
+      <h1>더 자세히 말씀해 주세요!</h1>
+      {/* 디테일 정보구성 컴포넌트 호출 */}
+      <DetailCarInfo model={model} color={color} opt={opt} />
+    </React.Fragment>
+  );
+} ////////// AskMoreInfo 컴포넌트 //////////
+
+/// 디테일 정보구성 기술하는 컴포넌트 ///
+function DetailCarInfo({ model, color, opt }) {
+  // info는 세부적인 모델정보 객체가 들어온다!
+  // 전달 속성은 model(모델명), color(색상), opt(css옵션)
+  return (
+    <React.Fragment>
+      <h2>
+        모델명은 {model}이고 자동차 색은 {color}입니다!
+      </h2>
+      {/* 이미지 출력 */}
+      <img src="./images/ray.png" alt="기아레이" style={opt} />
+      {/* 리액트 style 속성의 값으로 객체를 CSS속성에 맞게 주면
+      인라인코드로 CSS를 셋팅할 수 있다! */}
+    </React.Fragment>
+  );
+} ////////// DetailCarInfo 컴포넌트 //////////
+
+/// 전체 자동차 브랜드 소개 컴포넌트 ///
+function ShowBrandCar({ brand, modelNum }) {
+  return (
+    <React.Fragment>
+      <h1>당신의 차는 무슨 차죠?</h1>
+      <IntroCar brand={brand} modelNum={0} />
+    </React.Fragment>
+  );
+} ///////// ShowBrandCar 컴포넌트 /////////
+
+// 화면출력하기
+// ReactDOM.render(출력코드,출력대상)
+ReactDOM.render(
+  <div>
+    <ShowBrandCar brand="기아레이" modelNum={0} />
+    <ShowBrandCar brand="기아레이" modelNum={2} />
+    <ShowBrandCar brand="기아레이" modelNum={1} />
+  </div>,
+  mFn.qs("#root1")
+);
