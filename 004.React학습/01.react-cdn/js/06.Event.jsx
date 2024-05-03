fa6d88c88f2b2ad6548152ebdd9de8c2101a1f2d
Sujin <9_ight@naver.com>
2024-05-03 16:54:21 +0900

    Update 06.Event.jsx

--- 004.React학습/01.react-cdn/js/06.Event.jsx
+++ 004.React학습/01.react-cdn/js/06.Event.jsx
@@ -5,8 +5,8 @@ import mFn from "./my_function";
 
 /************************************************************* 
     [ 리액트 이벤트 ]
-    1. 일반 HTML DOM 이벤트와 마찬가지로 사용자이벤트 사용가능함
-    2. 이벤트 종류: click, change, mouseover 등 일반이벤트
+    1. 일반 HTML DOM 이벤트와 마찬가지로 사용자이벤트 사용 가능함
+    2. 이벤트 종류: click, change, mouseover 등 일반 이벤트
     3. 이벤트 표기법: 캐믈케이스 - 첫글자소문자 단어마다 대문자
       예) onclick -> onClick
     4. 이벤트 핸들러 : 중괄호 안에 작성(중괄호는 JSX표현식영역)
@@ -14,6 +14,10 @@ import mFn from "./my_function";
     5. 리액트에 속성형태로 등록하는 이벤트는 html요소에 등록된
       이벤트 속성과 달리 JS 이벤트리스너를 통한 이벤트 객체에
       등록되므로 html 태그상 이벤트 등록 속성이 보이지 않는다!
+
+    [ 컴포넌트 호출시 중요한 특징 하나! ]
+    ->>> 설정된 전달변수와 셋팅속성은 전달된 속성값만 셋팅된다!
+    쉽게 말해 보내주지 않은 속성은 스킵(skip!!!)된다는 말!
 *************************************************************/
 
 /////// 전체 이벤트 적용할 컴포넌트 구성하기 //////////////////
@@ -142,7 +146,8 @@ function EventShow() {
       ialt="페라리 레드"
       itit="클릭하면 시운전 해요!"
       idName="fcar"
-      clickFn={moveCar}
+      // 함수에 값을 보낼 때에는 익명함수로 처리!
+      clickFn={()=>moveCar("#fcar")}
       />,mFn.qs("#ferrari"));
     // ReactDOM.render(어쩌구,저쩌구);
     // 어쩌구를 저쩌구에 넣기
@@ -201,6 +206,20 @@ ReactDOM.render(<EventShow />, mFn.qs("#root"));
 
 
 // 일반함수로 페라리 움직이기 구현
-function moveCar(){
-  console.log("페라리 움직여!");
+function moveCar(eleName){
+  console.log("페라리 움직여!",eleName);
+
+  // 1. 대상요소 셋팅하기
+  const tg = mFn.qs(eleName).style;
+  // console.log(target);
+
+  // 2. 번갈아서 왔다갔다 움직이기
+  tg.translate =
+  tg.translate == "150%"?"0":"150%";
+  tg.scale =
+  tg.scale == "2"?"1":"2";
+  tg.transition = "2s ease-in-out";
+
+
+
 } ////////// moveCar 함수 //////////
