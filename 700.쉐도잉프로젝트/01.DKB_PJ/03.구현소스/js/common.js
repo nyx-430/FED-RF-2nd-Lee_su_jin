527051ec17881fc502b86aa2f7682a487be14fa4
Sujin <9_ight@naver.com>
2024-05-07 12:20:45 +0900

    0507/12:20

--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/common.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/common.js
@@ -6,13 +6,17 @@ import mFn from "./my_function.js";
 // 공통 처리 데이터 불러오기
 import comData from "../data/common_data.js";
 
-// 모듈로 호출된 JS에서는 다른 외부JS를 import로 호출가능!
-// import하려는 파일에서 반드시 함수,변수 등을 export해야함!
+// 모듈로 호출된 JS에서는 다른 외부 JS를 import로 호출 가능!
+// import하려는 파일에서 반드시 함수, 변수 등을 export해야 함!
 import slideFn from "./slide.js";
 
-// GNB 메뉴 데이터 불러오기 /////
+// GNB 메뉴 데이터 불러오기
 import gnbData from "../data/gnb_data.js";
-console.log(gnbData);
+// console.log(gnbData);
+
+// 콤보박스 데이터 불러오기
+import comboData from "../data/combo_data.js";
+console.log(comboData);
 
 export default function setElement() {
   // 1.대상선정: #top-area, #ban-area,
@@ -30,11 +34,14 @@ export default function setElement() {
 
   // 3. 기능처리 함수 호출하기
 
-  // 3-1. GNB메뉴 만들기 함수 호출!
+  // 3-1. GNB 메뉴 만들기 함수 호출
   makeMenu();
 
-  // 3-2. slideFn 슬라이드 기능함수 호출!
+  // 3-2. slideFn 슬라이드 기능함수 호출
   slideFn();
+
+  // 3-3. bindCombo 하단 콤보박스 바인딩 함수 호출
+  bindCombo();
 } /////////// setElement 함수 ///////////////
 
 // [ GNB메뉴코드 만들기 함수 ] ///////
@@ -88,3 +95,46 @@ function makeMenu() {
     
 `;
 } ////////// makeMenu 함수 //////////
+
+// 콤보박스 바인딩 함수
+function bindCombo(){
+  // 1. 대상 선정 : #brand, #corp
+  const brandBox = document.querySelector("#brand");
+  const corpBox = document.querySelector("#corp");
+  console.log("콤보 바인딩!",brandBox,corpBox);
+
+  // 2. 데이터 바인디하기
+  // 2-1. 브랜드 바로가기 콤보박스 : 단순 바인딩(option만)
+  // 데이터 대상 ; comboData.brand
+
+  // 대상 요소 내부 데이터 초기화
+  // 배열데이터 .map().join('');
+  brandBox.innerHTML = comboData.brand.map((v,i) => `
+    <option value="brand${i}">${v}</option>
+  `).join('');
+
+  // 2-2. 계열사 바로가기 콤보박스
+  // -> 복합 바인딩 : optgroup > option
+  // 데이터 분석 : 객체로 된 데이터이므로 map()을 쓰려면
+  // 객체의 키(key)를 배열로 추출하여 사용한다!
+  // Object.keys(객체) -> 키배열!
+  // 객체의 값을 사용할 경우 - 원본객체[키]
+
+  // 데이터 대상 : comboData.corp
+  const corpData = Object.keys(comboData.corp);
+  console.log(corpData);
+
+  // 데이터 만들어서 넣기
+  corpBox.innerHTML = corpData.map((v,i) => `
+    <optgroup label="${v}">
+    ${
+      // 해당 객체의 값은 키배열 값과 매칭함!
+      // ov변수는 객체가 가지는 배열값임
+      comboData.corp[v].map((ov,oi) => `
+      <option value="corp${i+1}-${oi+1}">${ov}</option>
+      `).join('')
+    }
+    </optgroup>
+  `).join('');
+
+} //////////// bindCombo 함수 ////////////
