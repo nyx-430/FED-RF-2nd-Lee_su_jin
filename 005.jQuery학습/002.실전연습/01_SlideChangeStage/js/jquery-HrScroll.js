da1a1ca0b0af0a595b5515cc26083c93a275b169
Sujin <9_ight@naver.com>
2024-05-16 12:40:24 +0900

    0516/12:40

--- 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
+++ 005.jQuery학습/002.실전연습/01_SlideChangeStage/js/jquery-HrScroll.js
@@ -7,44 +7,93 @@ const scTarget = $("html,body");
 // 제이쿼리에서 전체 스크롤/휠 이벤트 작성시 대상은 항상 html, body 두개 모두 잡아준다
 // 참고로 document나 window는 사용 안 됨!
 // 이벤트 메서드: on(이벤트명,함수)
+// 유의사항: 휠 이벤트는 모바일과 무관하다!
+// 따라서 모바일 터치 가로 스크롤은 그냥 가로 스크롤바만 살려주면 자연스럽게 된다!
 
 // 스크롤 위치값 변수
 let scPos = 0;
 
-let winW = $(window).width();
+// 페이지 개수
 let pgCnt = $(".page").length;
 
-// 최대한계값 : 전체 이동박스 크기 - 화면 가로 크기
-let maxLimit = (winW*pgCnt) - winW;
+// winW : 윈도우 가로 크기
+// maxLimit : 최대한계값
+let winW, maxLimit;
 
-console.log("window 크기:",winW,"/페이지 수:",pgCnt,"/최대한계값:",maxLimit);
+// 한번에 움직일 스크롤 이동 크기
+// 상수는 모두 대문자로 쓰고 중간에 언더바로 구분
+const MOVE_NUM = 100;
 
-scTarget.on("wheel",(e)=>{
-    // 스크롤 이동을 위한 제이쿼리 속성
-    // 1. scrollTop: 세로 스크롤바 위치
-    // 2. scrollLeft: 가로 스크롤바 위치
-    
-    // scPos += 200;
-    // scPos = scPos + 200;
-    
-    // 휠 방향 알아내기 (전체 이벤트 객체로부터 얻어옴)
-    let delta = event.wheelDelta;
+// 한계값 계산 함수 ///////////////////////////////
+const chgLimit = () => {
+  winW = $(window).width();
+  // 최대한계값 : 전체 이동박스 크기 - 화면 가로 크기
+  maxLimit = winW * pgCnt - winW;
 
-    if(delta < 0) scPos += 200;
-    else scPos -= 200;
+  console.log(
+    "window 크기:",
+    winW,
+    "/페이지 수:",
+    pgCnt,
+    "/최대한계값:",
+    maxLimit
+  );
+}; //////////////// chgLimit 함수 ////////////////
 
-    // 한계값 체크
-    // 최소한계 : 0
-    if(scPos <= 0) scPos = 0;
-    // 최대한계 : 전체 이동박스 크기 - 화면 가로 크기
-    if(scPos >= maxLimit) scPos = maxLimit;
-    
-    console.log("스크롤 위치:",scPos,delta);
+// 윈도우 사이즈 변경시 한계값 업데이트
+// resize() 메서드: 리사이즈 이벤트 함수
+$(window).resize(chgLimit);
 
-    // animate({CSS설정},시간,이징,함수)
-    // stop() 메서드: 큐에 쌓인 애니메이션을 지움
-    // 중간에 쌓인 애니를 지우고 최종 애니만 실행한다
-    scTarget.stop().animate({scrollLeft: scPos+"px"},500)
+// 최대한계값 함수 최초 호출
+chgLimit();
 
-    
-}); /////////// wheel 이벤트 구역 ///////////
+//////////////////////////////////////////////////
+// 스크롤 이벤트 함수 구현하기 /////////////////////
+scTarget.on("wheel", (e) => {
+  // 스크롤 이동을 위한 제이쿼리 속성
+  // 1. scrollTop: 세로 스크롤바 위치
+  // 2. scrollLeft: 가로 스크롤바 위치
+
+  // scPos += 200;
+  // scPos = scPos + 200;
+
+  // 휠 방향 알아내기 (전체 이벤트 객체로부터 얻어옴)
+  // (1) event.wheelDelta 값: 기본 이동 100px + 앞뒤 예비 공백 10px*2 = 20px
+  // 전체 표시 수치 120 -> 120px을 의미
+  // 방향은 마이너스가 아랫 방향
+  // let delta = event.wheelDelta;
+  
+  // (2) event.deltaY
+  // 기본 이동 크기만 표시함
+  // 즉, 100px 이동값 표시, 방향은 양수가 아랫 방향
+  // -> wheelDelta보다 나중에 나온 실질적인 표시 객체
+  let delta = event.deltaY;
+
+  // 방향에 따를 증감은 deltaY는 양수가 아랫 방향
+  // wheelDelta은 음수가 아랫 방향
+  if (delta > 0) scPos += MOVE_NUM;
+  else scPos -= MOVE_NUM;
+
+  // 한계값 체크
+  // 최소한계 : 0
+  if (scPos <= 0) scPos = 0;
+
+  // 최대한계 : 전체 이동박스 크기 - 화면 가로 크기
+  if (scPos >= maxLimit) scPos = maxLimit;
+
+  console.log("스크롤 위치:", scPos, delta);
+
+  // animate({CSS설정},시간,이징,함수)
+  // stop() 메서드: 큐에 쌓인 애니메이션을 지움
+  // 중간에 쌓인 애니를 지우고 최종 애니만 실행한다
+  scTarget.stop().animate(
+    {
+      scrollLeft: scPos + "px",
+    },
+    // 시간(1/1000초)
+    2000,
+    // 이징(가속도: 처음에 빠르게 나중에 천천히)
+    "easeOutQuart"
+  );
+}); ////////////// wheel 이벤트 구역 //////////////
+//////////////////////////////////////////////////
--- 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
+++ 700.쉐도잉프로젝트/01.DKB_PJ/03.구현소스/js/main.js
@@ -28,7 +28,8 @@ setTimeout(showSubBox);
 
 ///////////////////////////////////////////////
 
-/// 구현코드 파트 /////////////////////////////
+/// 구현코드 파트 //////////////
+
 // 1. 부드러운 스크롤 호출
 const mySmooth = new SmoothScroll(document, 30, 20);
 
@@ -39,7 +40,7 @@ const mySmooth = new SmoothScroll(document, 30, 20);
 // 이벤트 대상 === 변경대상 : .intro-mv-img
 const introMv = myFn.qs(".intro-mv-img");
 introMv.onclick = () => {
-  console.log("인트로영상!!!");
+  // console.log("인트로영상!!!");
   // 1. 동영상 넣기
   introMv.innerHTML = `<video src="./images/intro_mv.mp4" 
     autoplay controls></video>`;
@@ -85,34 +86,38 @@ introMv.onclick = () => {
 
   // 1. 8개만 데이터를 html로 구성하여 넣는다!
   // html 코드변수
-  //let hcode = `<ul class="fx-box">`;
+  // let hcode = `<ul class="fx-box">`;
 
-  //// li구성을 hcode변수에 대입연산자로 할당함!
-  //for (let i = 0; i < 8; i++) {
-  //  hcode += `
-  //      <li>
-  //          <h3>${pData[i].title}</h3>
-  //          <p>${pData[i].story}</p>
-  //      </li>
-  //  `;
-  //} //// for //////
+  // li구성을 hcode변수에 대입연산자로 할당함!
+  // for (let i = 0; i < 8; i++) {
+  //   hcode += `
+  //       <li>
+  //           <h3>${pData[i].title}</h3>
+  //           <p>${pData[i].story}</p>
+  //       </li>
+  //   `;
+  // } //// for //////
 
-  //hcode += `</ul>`;
+  // hcode += `</ul>`;
 
   // 데이터 확인
   // console.log(hcode);
   // console.log('대상:',previewBox,'미리보기 data:',pData);
 
-  // 2. 화면출력하기 -> map()으로 한꺼번에 출력하자!
+  // 2. 화면출력하기 -> map()으로 한번에 출력하자! ///////
   previewBox.innerHTML = `
-  <ul class="fx-box">
-    ${pData.map(v=>`
-      <li data-idx="${v.idx}">
-        <h3>${v.title}</h3>
-        <p>${"방송일 : "+v.date+" "+v.story}</p>
-      </li>
-    `).join('')}
-  </ul>
+    <ul class="fx-box">
+      ${pData
+        .map(
+          (v) => `
+        <li data-idx="${v.idx}">
+            <h3>${v.title}</h3>
+            <p>${"방송일 : " + v.date + " " + v.story}</p>
+        </li>      
+      `
+        )
+        .join("")}
+    </ul>
   `;
 })(); //// 미리보기 코드랩핑구역 종료 /////////
 
@@ -130,7 +135,7 @@ introMv.onclick = () => {
   let hcode = `<ul>`;
 
   // li구성을 hcode변수에 대입연산자로 할당함!
-  // liveData 배열은 총 8개임. 모두 돌기를 셋팅하자!
+  // liveData 배열은 총8개임. 모두 돌기를 셋팅하자!
   lvData.forEach((v) => {
     hcode += `
               <li>
@@ -146,7 +151,7 @@ introMv.onclick = () => {
 
   // 데이터 확인
   // console.log(hcode);
-  // console.log('대상:',liveBox,'현장포토 data:',lvData);
+  //   console.log('대상:',liveBox,'현장포토 data:',lvData);
 
   // 2. 화면출력하기 ///////
   liveBox.innerHTML = hcode;
@@ -190,16 +195,16 @@ introMv.onclick = () => {
 
 // 5. 최신동영상 파트 데이터 태그 구성하여 화면출력하기 ///
 // 코드랩핑구역 시작 //////////////////////////
-(()=>{
+(() => {
   // 5-1. 변경대상: .clip-box
-  const clipBox = myFn.qs('.clip-box');
+  const clipBox = myFn.qs(".clip-box");
 
   // 5-2. 생성코드 변수
   let hcode = `<ul class="slide">`;
 
   // 데이터만큼 순회하여 li코드 만들기 ///
   // 데이터: dkbData.clipData
-  dkbData.clipData.forEach(v=>{
+  dkbData.clipData.forEach((v) => {
     hcode += `
     <li>
       <div class="clip-mv-box">
@@ -212,66 +217,88 @@ introMv.onclick = () => {
       <h3>${v.title}</h3>
     </li>
     `;
-
   }); /////////// forEach /////////////////
 
   hcode += `</ul>`;
 
   // 5-3. 화면출력하기 ///////
   clipBox.innerHTML = hcode;
-
-
 })();
 // 코드랩핑구역 종료 //////////////////////////
 
 // 드래그 슬라이드 태그 구성후 호출하기!
-setSlide('banbx');
+setSlide("banbx");
 
-/*************************************************** 
-  메인페이지용 도깨비 메뉴 스크롤 이동 제이쿼리 구현
-***************************************************/
-// 메뉴 클릭 대상 : .spart-menu a
-$(".spart-menu a").click(e=>{
-  // a요소 클릭시 기본 이동 막기
+/**************************************************** 
+  메인 페이지용 도깨비 메뉴 스크롤이동 제이쿼리 구현 
+****************************************************/
+// 메뉴 클릭 대상: .spart-menu a
+$(".spart-menu a").click((e) => {
+  // a 요소 클릭시 기본이동 막기
   e.preventDefault();
 
   // 1. 클릭한 a요소의 글자 읽어오기
   let txt = $(e.target).text();
-  console.log(txt);
+  // console.log(txt);
 
   // 2. 이동할 위치값 알아내기
   let pos;
   // 2-1. 이동할 위치의 박스 아이디 매칭하기
-  switch (txt){
-    case "미리보기":pos = "#preview-area"; break;
-    case "프로그램 소개":pos = "#intro-area"; break;
-    case "동영상":pos = "#clip-video-area"; break;
-    case "현장 포토":pos = "#real-photo-area"; break;
-    case "대표 포스터":pos = "#main-photo-area"; break;
-  } /// switchcase ///
+  switch (txt) {
+    case "미리보기":
+      pos = "#preview-area";
+      break;
+    case "프로그램 소개":
+      pos = "#intro-area";
+      break;
+    case "동영상":
+      pos = "#clip-video-area";
+      break;
+    case "현장 포토":
+      pos = "#real-photo-area";
+      break;
+    case "대표 포스터":
+      pos = "#main-photo-area";
+      break;
+  } ///////// switch case ////////
 
-  // 만약 해당된 요소가 없으면 여기도 돌아가!
-  // 위에서 할당 안되면 undefined이면 if문에서 false 처리됨
-  // !(NOT)연산자로 반대로 뒤집으면 false일 때 처리함!
-  if(!pos) return;
+  // 만약 해당된 요소가 없으면 여기서 돌아가!
+  // 위에서 할당안되면 undefined이면 if문에서 false처리됨
+  // !(NOT)연산자로 반대로 뒤집으면 false일때 처리함!
+  if (!pos) return;
 
   // 2-2. 해당 박스 아이디의 위치값 알아내기
-  // offset().top은 제이쿼리 top위치값 정보
+  // offset().top 제이쿼리 top 위치값정보
   pos = $(pos).offset().top;
-  console.log("위치값:",pos);
+  // console.log("위치값:", pos);
 
   // 3. 스크롤 애니메이션 이동하기
-  // 제이쿼리는 이것을 정말 잘한다!
-  // $("html, body").animate({scrollTop:몇px},시간,이징,함수)
-  $("html, body")
-  .animate({scrollTop:pos+"px"},
-  800, // 시간
-  "easeInSine", // 이징 (http://easings.net)
-  // 이동 후 부드러운 스크롤 위치값 업데이트 필수!
-  ()=>{
-    // 이것 안 하면 위치 이동 후 스크롤시 튐!
-    setScrollPos(pos);
-  }
-  ); 
+  // 제이쿼리는 이것을 정말 잘한다!!!
+  // $("html,body").animate({scrollTop:몇px},시간,이징,함수)
+  $("html,body").animate(
+    { scrollTop: pos + "px" },
+    800, // 시간(1/1000초)
+    "easeInOutQuint", // 이징(https://easings.net)
+    // 콜백함수(애니후 호출되는 함수)
+    () => {
+      // 이동후 부드러운 스크롤 위치값 업데이트 필수
+      // 이것 안하면 위치이동후 스크롤시 튐!
+      // 생성자함수 하위 객체변수로 등록된 함수를 호출함!
+      mySmooth.setScrollPos(pos);
+    }
+  );
+}); //////// 도깨비 파트 메뉴 클릭 함수 ///////////////
 
-}); ////////// 도깨비 파트 메뉴 클릭 함수 //////////
+// 개별 박스에 부드러운 스크롤 생성자함수 적용하기연습
+// $(".preview-box").css({
+//   height: "200px",
+//   overflow: "auto"
+// })
+// .on("wheel",e=>{
+//   e.stopPropagation();
+// })
+
+// 부드러운 스크롤 개별박스 적용
+// const smallSmooth =
+// new SmoothScroll(
+//   myFn.qs(".preview-box"),20,30);
