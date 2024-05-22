5baf3636333312d614381d07de8bac1f2a661e09
Sujin <9_ight@naver.com>
2024-05-22 13:16:01 +0900

    0522/13:15

--- 003.JS학습/JS/my_function.js
+++ 003.JS학습/JS/my_function.js
@@ -1,9 +1,10 @@
 // 공통함수 JS - my_function.js
 
 // 객체를 내보내기 : 변수선언과 이름없이 바로 직접 내보냄!
-// const myFn = 
+// export default {
+
+const mFn = {
 // -> 선언과 할당후 export default하려면 하단에서 해야함!
-export default {
     // 요소선택함수 ////////
     qs: (x) => document.querySelector(x),
     qsEl: (el, x) => el.querySelector(x),
@@ -19,3 +20,6 @@ export default {
     // 옵셋탑값 반환함수
     getOT: (ele) => ele.offsetTop,
   };
+
+// 내보내기
+export default mFn;
