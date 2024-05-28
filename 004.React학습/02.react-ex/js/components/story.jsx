1d91aa703f0f09532f526c06834f81432f5e5d77
Sujin <9_ight@naver.com>
2024-05-28 17:40:19 +0900

    0528/17:40

--- 004.React학습/02.react-ex/js/components/story.jsx
+++ 004.React학습/02.react-ex/js/components/story.jsx
@@ -6,16 +6,27 @@ import { 누구냐 } from "./provider";
 // 산 정보 불러오기
 import { mtInfo } from "../data/mt_data";
 
+// 산 아이콘 불러오기
+import MtIcon from "./mt_Icon";
+
 export default function 이야기() {
   // 컨텍스트를 사용하려면 useContext() 메서드 사용
   const 나야나 = React.useContext(누구냐);
 
   // 산 정보는 배열이므로 순회하여 해당 데이터를 할당함
-  const selMtInfo = mtInfo.find(v=>{
-    if(v.이름==나야나.mtName) return true;
+  const selMtInfo = mtInfo.find((v) => {
+    if (v.이름 == 나야나.mtName) return true;
   });
 
-  console.log(selMtInfo);
+//   console.log(selMtInfo);
+
+  // 버튼 만들기에 사용할 산 이름 배열 만들기
+  // 기존 산 정보 객체의 배열에서 산 이름값만 모아서
+  // 새로운 배열을 만든다!
+  // 이것이 .map() 메서드의 본래 사용법이다!
+  const mtTotal = mtInfo.map((v) => v.이름);
+
+  console.log("산 이름만 새 배열:", mtTotal);
 
   // 산 이름 변경 메서드
   const changeMtName = (e) => {
@@ -28,30 +39,45 @@ export default function 이야기() {
   // 코드 리턴 구역
   return (
     <div style={나야나.mtBoxCss}>
-        {/* 1. 산 이름 타이틀 */}
-      <h1>{나야나.mtName}</h1>
-        {/* 2. 산 이미지 */}
-        <img 
-        src={selMtInfo.이미지} 
+      {/* 1. 산 이름 타이틀 */}
+      <h1>
+        {나야나.mtName != "후지산" && 
+        <MtIcon mtName={나야나.mtName} />}
+        {나야나.mtName}
+        {나야나.mtName != "후지산" && 
+        <MtIcon mtName={나야나.mtName} />}
+      </h1>
+      {/* 2. 산 이미지 */}
+      <img
+        src={selMtInfo.이미지}
         alt={selMtInfo.이름}
-        style={{width:"100%"}}
-        />
-        {/* 3. 산 정보 박스 */}
-        <div style={나야나.mtInfoBoxCss}>
-            <ul>
-                <li>이름 : {selMtInfo.이름}</li>
-                <li>설명 : {selMtInfo.설명}</li>
-                <li>높이 : {selMtInfo.높이}</li>
-                <li>융기 : {selMtInfo.융기}</li>
-                <li>최초등반 : {selMtInfo.최초등반}</li>
-                <li>최초등반가 : {selMtInfo.최초등반가}</li>
-                <li>산맥 : {selMtInfo.산맥}</li>
-            </ul>
-        </div>
-
-      <button onClick={changeMtName}>후지산</button>
-      <button onClick={changeMtName}>에베레스트산</button>
-      <button onClick={changeMtName}>백두산</button>
+        style={{ width: "100%" }}
+      />
+      {/* 3. 산 정보 박스 */}
+      <div style={나야나.mtInfoBoxCss}>
+        <ul>
+          <li>◎ 이름 : {selMtInfo.이름}</li>
+          <li>◎ 설명 : {selMtInfo.설명}</li>
+          <li>◎ 높이 : {selMtInfo.높이}</li>
+          <li>◎ 융기 : {selMtInfo.융기}</li>
+          <li>◎ 최초등반 : {selMtInfo.최초등반}</li>
+          <li>◎ 최초등반가 : {selMtInfo.최초등반가}</li>
+          <li>◎ 산맥 : {selMtInfo.산맥}</li>
+        </ul>
+      </div>
+      {/* 4. 현재 산을 제외한 나머지 산 버튼 생성하기 */}
+      {mtTotal.map(
+        (v) =>
+        // 현재 산 이름(mtName)이 아닌 배열값만 버튼 생성
+          v != 나야나.mtName && (
+            <button
+              style={{ padding: "15px", fontSize: "20px", margin: "10px" }}
+              onClick={changeMtName}
+            >
+              {v}
+            </button>
+          )
+      )}
     </div>
   );
 } /////////// 이야기 컴포넌트 ///////////
