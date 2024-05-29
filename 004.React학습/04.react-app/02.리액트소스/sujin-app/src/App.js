e3272a0956952fdd649e88182ea004555c14dfff
Sujin <9_ight@naver.com>
2024-05-29 16:46:44 +0900

    0529/16:46

---
+++ 004.React학습/04.react-app/02.리액트소스/sujin-app/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+import $ from "jquery";
+import { useEffect } from 'react';
+
+function App() {
+  useEffect(()=>{
+    $(".App-header span").hover( // 호버시
+      (e)=>{
+      $(e.currentTarget).stop().animate(
+        {scale: 1.4}
+        ,500);
+  },
+      (e)=>{ // 아웃시
+      $(e.currentTarget).animate(
+        {scale: 1}
+        ,500);
+  })
+  },[])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <span>
+          <img src="https://asiacatexpo.com/wp-content/uploads/2020/12/red-white-cat-i-white-studio2-768x646.png"
+          className="App-logo" alt="logo" />
+        </span>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
