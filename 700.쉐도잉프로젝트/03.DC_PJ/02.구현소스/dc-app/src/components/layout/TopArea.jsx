431941d25dd5025668fb6f39be55c6ba2b551a1b
Sujin <9_ight@naver.com>
2024-06-04 16:42:39 +0900

    0604/16:42

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -2,34 +2,58 @@
 
 // GNB 데이터 불러오기
 import { Link } from "react-router-dom";
-import {menu} from "../data/gnb.js";
+import { menu } from "../data/gnb.js";
 
 // 상단영역 CSS 불러오기
 import "../../css/top_area.scss";
 import Logo from "../modules/Logo.jsx";
 
-export default function TopArea (){
-    // 코드 리턴 구역 ////////////
-    return (
-        <>
-        {/* 1.상단영역 */}
-        <header className="top-area">
-            {/* 로그인 환영메시지 박스 */}
-            {/* 네비게이션 GNB파트 */}
-            <nav className="gnb">
-                <ul>
-                  {/* 1. 로고 컴포넌트 */}
-                  <li>
-                    <Logo logoStyle="top" />
-                  </li>
-                  {/* 2. GNB메뉴  */}
-                  {menu.map((v,i)=>
-                  <li key={i}>
+export default function TopArea() {
+  // 코드 리턴 구역 ////////////
+  return (
+    <>
+      {/* 1.상단영역 */}
+      <header className="top-area">
+        {/* 로그인 환영메시지 박스 */}
+        {/* 네비게이션 GNB파트 */}
+        <nav className="gnb">
+          <ul>
+            {/* 1. 로고 컴포넌트 */}
+            <li>
+              <Logo logoStyle="top" />
+            </li>
+            {/* 2. GNB메뉴 데이터 배열로 만들기 */}
+            {menu.map((v, i) => (
+              <li key={i}>
+                {
+                  // 하위 메뉴가 있으면 일반 a요소에 출력
+                  // 없으면 Link 라우팅 출력
+                  v.sub ? (
+                    <a href="#">{v.txt}</a>
+                  ) : (
                     <Link to={v.link}>{v.txt}</Link>
-                  </li>)}
-                </ul>
-            </nav>
-        </header>
-        </>
-    );
+                  )
+                }
+                {
+                  // 서브 메뉴 데이터가 있으면 하위 그리기
+                  v.sub && (
+                    <div className="smenu">
+                      <ol>
+                        {v.sub.map((v, i) => (
+                          <li key={i}>
+                            <Link to={v.link}>{v.txt}</Link>
+                          </li>
+                        ))}
+                      </ol>
+                    </div>
+                  )
+                }
+                <Link to={v.link}>{v.txt}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 } ////////////// TopArea //////////////
