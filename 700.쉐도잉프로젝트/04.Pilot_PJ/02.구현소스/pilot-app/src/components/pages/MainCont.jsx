5d03366d0a10e565e6b347deff6bdcd750c17b5a
Sujin <9_ight@naver.com>
2024-06-14 11:21:18 +0900

    0614/11:21

---
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/pages/MainCont.jsx
@@ -0,0 +1,69 @@
+// 메인페이지 컨텐츠 컴포넌트 - MainCont.jsx
+
+import React, { useEffect, useLayoutEffect } from "react";
+import Banner from "../modules/Banner";
+
+// 자동휠 함수 불러오기
+import * as wFn from "../../js/func/auto_wheel";
+
+function MainCont (props){
+
+  // 화면 랜더링 실행 구역 //////////
+  useEffect(()=>{
+    console.log("useEffect");
+
+  },[]);
+  // useLayoutEffect(()=>{
+  //   console.log("useLayoutEffect");
+  // },[]);
+
+  // 코드 리턴 구역 //////////
+  return (
+    <>
+      {/* 1. 배너 컴포넌트 */}
+      <section
+        id="ban"
+        className="page none-sel"
+        style={{ background: "lightblue" }}
+      >
+        <Banner />
+      </section>
+      {/* 2. 남성패션 영역 */}
+      <section className="page"></section>
+
+      {/* 3. 여성패션 영역 */}
+      <section className="page"></section>
+
+      {/* 4. 스타일패션 영역 */}
+      <section className="page"></section>
+
+      {/* 메인에만 나오는 사이드 인디케이터 */}
+      <nav className="indic">
+        <ul>
+          <li className="on">
+            <a href="#ban">
+              <span>배너</span>
+            </a>
+          </li>
+          <li>
+            <a href="#men">
+              <span>남성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#women">
+              <span>여성의류</span>
+            </a>
+          </li>
+          <li>
+            <a href="#style">
+              <span>스타일</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default MainCont;
--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
+import TopArea from './components/layout/TopArea';
+import MainArea from './components/layout/MainArea';
+import FooterArea from './components/layout/FooterArea';
+
+// 전체 공통 CSS
+import "./css/index.scss";
 
 function MainComponent(props) {
   return (
-    <div>
-      <h1>파일럿 PJ입니다!</h1>
-    </div>
+    <>
+      <TopArea />
+      <MainArea />
+      <FooterArea />
+    </>
   );
 }
 
-// 출력하기
-const root = createRoot(document.querySelector("#root"));
+// 출력하기 //////
+const root = createRoot(document.querySelector("#root"))
 root.render(<MainComponent />);
