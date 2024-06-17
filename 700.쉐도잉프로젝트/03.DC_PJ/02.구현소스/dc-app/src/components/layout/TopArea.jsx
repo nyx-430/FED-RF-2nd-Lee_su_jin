291a7367c698a4f78ad098229cb8252a2dccdb92
Sujin <9_ight@naver.com>
2024-06-17 11:52:21 +0900

    0617/11:52

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/components/layout/TopArea.jsx
@@ -1,7 +1,7 @@
 // 상단영역 컴포넌트 ///
 
 // GNB 데이터 불러오기
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { menu } from "../data/gnb";
 
 // 상단영역 CSS 불러오기
@@ -9,7 +9,21 @@ import "../../css/top_area.scss";
 import Logo from "../modules/Logo";
 
 export default function TopArea() {
-  //// 코드 리턴구역 //////////////
+  // 이동 함수
+  const goNav = useNavigate();
+  // 사용시 goNav(라우터주소,{전달객체})
+  // 전달객체 없으면 비워놓음
+  // 사용법: 반드시 useNavigate()메서드를 변수에 담아
+  // 이동할 라우터 주소를 쓰면 이동한다
+  // 예) goNav('/News') - News 페이지 이동
+  // 예) goNav('/nEws') - News 페이지 이동
+  // 예) goNav('/') - 첫 페이지로 이동
+  // 예) goNav('') - 첫 페이지로 이동
+  // 이동 주소는 대소문자 구분 안 함!
+  // 슬래쉬(/) 없어도 루트로 인식함
+  // -> 빈값이면 루트로 이동함!
+
+  // 코드 리턴 구역 //////////////
   return (
     <>
       {/* 1.상단영역 */}
@@ -21,9 +35,16 @@ export default function TopArea() {
           <ul>
             {/* 1. 로고 컴포넌트 */}
             <li>
-              <Link to="/">
+              <a href="#" onClick={(e) => {
+                // 기본 이동 막기
+                e.preventDefault();
+                // 페이지 라우터 이동 메서드 호출
+                goNav("/")}}>
                 <Logo logoStyle="top" />
-              </Link>
+              </a>
+              {/* <Link to="/">
+                <Logo logoStyle="top" />
+              </Link> */}
             </li>
             {/* 2. GNB메뉴 데이터 배열로 만들기 */}
             {menu.map((v, i) => (
@@ -38,7 +59,7 @@ export default function TopArea() {
                   )
                 }
                 {
-                  // 서브 메뉴 데이터가 있으면 하위그리기
+                  // 서브 메뉴 데이터가 있으면 하위 그리기
                   v.sub && (
                     <div className="smenu">
                       <ol>
@@ -58,4 +79,4 @@ export default function TopArea() {
       </header>
     </>
   );
-} /////////// TopArea /////////////////////
+} /////////// TopArea ///////////
