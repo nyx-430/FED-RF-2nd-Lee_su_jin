2d24b4744c90b00c3d844087a5ea265eabba5d55
Sujin <9_ight@naver.com>
2024-06-17 13:14:36 +0900

    0617/13:14

--- 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/TopArea.jsx
+++ 700.쉐도잉프로젝트/04.Pilot_PJ/02.구현소스/pilot-app/src/components/layout/TopArea.jsx
@@ -2,8 +2,49 @@ import React from "react";
 
 // 메뉴 데이터 불러오기
 import { gnbData } from "../../js/data/gnb";
+import { TotalMenu } from "../modules/TotalMenu";
+
+// 제이쿼리
+import $ from "jquery";
 
 function TopArea(props) {
+  // 전체 메뉴 열기/닫기 함수
+  const showHideMenu = (e) => {
+    // console.log(e.currentTarget);
+
+    // 1. 보이기 숨기기 전환하기
+    // 전체 메뉴 대상 : .mbox
+    $(".mbox").fadeToggle(300);
+    // fadeIn() 서서히 나타남
+    // fadeOut() 서서히 사라짐 -> display: none
+    // fadeToggle() 서서히 나타나고 사라짐 전환
+
+    // 2. 햄버거 버튼에 클래스 "on" 넣고 빼기
+    $(e.currentTarget).toggleClass("on");
+    // addClass() 클래스 넣기
+    // removeClass() 클래스 빼기
+    // toggleClass() 클래스 넣기 또는 빼기
+
+    console.log($(e.currentTarget).is(".on"));
+
+    // 3. 비디오 재생/멈춤 하기
+    // 대상 : .bgm
+    let bgm = $(".bgm");
+    // 제이쿼리의 미디어를 선택 후 실제 사용할 때는
+    //get(0)하고 난 후 사용한다!
+
+    // 햄버거 버튼에 클래스 "on" 있으면 재생, 없으면 멈춤
+    $(e.currentTarget).is(".on")?
+    bgm.get(0).play()
+    :bgm.get(0).pause();
+    // play()는 재생, pause()는 멈춤 메서드
+
+    // 비교해서 JS는 선택 후 바로 play(), pause()를 사용함
+    // document.querySelector(".bgm").play();
+    // document.querySelector(".bgm").pause();
+  }; ////////// showHideMenu //////////
+
+  // 코드 리턴 구역 //////
   return (
     <>
       <div id="top-area">
@@ -15,7 +56,7 @@ function TopArea(props) {
           </h1>
           <nav className="gnb">
             <ul>
-              <li className="bld">배너순번 li 숨기기</li>
+              <li className="bld">배너 순번 li 숨기기</li>
               {gnbData.main.map((v, i) => (
                 <li key={i}>
                   <a href="#">{v}</a>
@@ -23,10 +64,11 @@ function TopArea(props) {
               ))}
             </ul>
           </nav>
-          <div className="ham">
+          <div className="ham" onClick={showHideMenu}>
             <span></span> <span></span> <span></span>
           </div>
-          {/* 전체메뉴 컴포넌트 */}
+          {/* 전체 메뉴 컴포넌트 */}
+          <TotalMenu />
         </header>
       </div>
     </>
