945221f4002297873a67c7f3da661bebbb022655
Sujin <9_ight@naver.com>
2024-06-25 10:20:18 +0900

    10:20

--- 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
+++ 700.쉐도잉프로젝트/03.DC_PJ/02.구현소스/dc-app/src/index.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import Character from "./components/pages/Character";
 import Movies from "./components/pages/Movies";
@@ -58,6 +58,8 @@ export default function MainComponent() {
     // basename속성은 pakage.json의 "homepage"속성값을 읽어옴
     // 읽는 방법은 process.env.PUBLIC_URL
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      {/* 라우터 경로 변경시 최상단 이동 컴포넌트 */}
+    <ScrollTop />
       <Routes>
         {/* 중요!!! 레이아웃 컴포넌트를 루트로 설정!
         루트 Route 는 홀로 닫지 말고 반드시 다른
@@ -83,6 +85,32 @@ export default function MainComponent() {
     </BrowserRouter>
   );
 }
+
+/*******************************************
+  컴포넌트로 만들고 라우터 안에 넣고
+  라우터 경로 변경시 스크롤 최상단 이동
+******************************************/
+const ScrollTop = () => {
+
+  // 라우터 경로 변경시 path값 읽어오기
+  // pathname객체 속성에 담긴다!
+  const {pathname} = useLocation();
+
+  // 화면 랜더링 구역에 스크롤 상단 이동 코드 넣기
+  useEffect(()=>{
+    window.scrollTo(0,0);
+    // 변경된 라우터 경로값 확인
+    console.log("라우터 경로:",pathname);
+
+  },[pathname]);
+  // 의존성을 라우터 경로 변수로 설정한다!
+
+  // 컴포넌트 리턴이 필요하나 소스 리턴이 아니므로
+  // null을 쓴다
+  return null;
+
+
+}; /////////// ScrollTop 컴포넌트 ///////////
 
 /// 컴포넌트 출력 ///
 // 먼저 root 객체 만들기
