b96969a5571e641ecf0768a1662222b4ef89c323
Sujin <9_ight@naver.com>
2024-08-05 13:32:47 +0900

    Update nanda.js

--- 007.VueJS학습/01.인트로/js/js/nanda.js
+++ 007.VueJS학습/01.인트로/js/js/nanda.js
@@ -10,6 +10,6 @@ const vm = new Vue({
         // (2-1) 샵명 데이터
         bigTit: "Style NANDA",
         // (2-2) 로고 태그 정보
-        logo: `<img src="./images/logo_3ce.png" alt="nanda logo" />`
+        logo: `<img src="./images/logo_3ce.png" alt="nanda logo">`
     },
 }); ////// vue //////
