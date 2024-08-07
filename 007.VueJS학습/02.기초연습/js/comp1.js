aadc672376b32383aad72fd9de488de2c6613a59
Sujin <9_ight@naver.com>
2024-08-07 11:17:02 +0900

    Update comp1.js

--- 007.VueJS학습/02.기초연습/js/comp1.js
+++ 007.VueJS학습/02.기초연습/js/comp1.js
@@ -22,22 +22,62 @@ Vue.component("tit-comp", {
 // 뷰인스턴스 생성하기
 makeVue(".tit");
 
+///////// [ 갤러리 리스트용 변수 셋팅 ] /////////
+// (1) 갤러리 이미지 번호용 변수
+let inum = 0;
+// (2) 상품명 배열
+const goods = ["프레이컷", "아일렛기모", "베어부클", "포멀믹스톤"];
+
 // 2. 갤러리 리스트에 넣을 전역 컴포넌트 만들기
-Vue.component("list-comp",{
-    // (2-1) template 옵션 : 태그 구성
-    // v-bind:속성 디렉티브 변수를 사용할 수 있는 속성 재구성해 줌
-    template: `
+Vue.component("list-comp", {
+  // (2-1) template 옵션 : 태그 구성
+  // v-bind:속성 -> 디렉티브 변수를 사용할 수 있는 속성 재구성해 줌
+  template: `
     <div>
-      <img v-bind:src="
-      './images/' + n + '.jpg'
-      " alt="의류아이템">
+      <img v-bind:src="gsrc" alt="의류아이템">
       <aside>
-        <h2>aa</h2>
-        <h3>qq</h3>
+        <h2 v-text="gname"></h2>
+        <h3 v-text="gprice"></h3>
       </aside>
     </div>
-    `,
-}); ////// component //////
+    `, /// template ///
+
+  // (2-2) data 옵션 : 컴포넌트 내부 변수 셋팅
+  // 실행 원리 : 컴포넌트가 빌드할 때 data 속성의 함수를 호출한다
+  // 그래서 함수의 리턴되는 객체값이 컴포넌트 내부에 전달된다
+  // data: function(){}
+  data() {
+    // 객체 리턴 필수!!!(중요!!!)
+    return {
+      // 이미지 경로
+      gsrc: `./images/${this.setNum()}.jpg`,
+      // 상품명
+      gname: this.setName(),
+      // 가격
+      gprice: this.setPrice(),
+    };
+  }, /// data ///
+  // (2-3) methods 속성 : 컴포넌트 내부 메서드 셋팅
+  methods: {
+    // [1] 이미지 번호 만들기 함수
+    // 외부 전역변수 inum을 1씩 증가하여 리턴함
+    setNum(){return ++inum},
+
+    // [2] 상품명 만들기 함수
+    setName(){return goods[Math.floor(Math.random()*4)]},
+
+    // [3] 가격 만들기 함수
+    setPrice() {
+        let rdm = Math.ceil(Math.random() * 17) + 3;
+        return this.addCommas(20000 * rdm) + "원";
+      },
+
+      // [4] 세자리 콤마 함수
+      addCommas(x) {
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+      },
+  }, /// methods ///
+}); //////////// component ////////////
 
 // 뷰인스턴스 생성하기
 makeVue(".grid");
