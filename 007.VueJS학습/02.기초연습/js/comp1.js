6f8c118721eeb0968aa5880b9ae6bb160353c9cf
Sujin <9_ight@naver.com>
2024-08-07 10:53:46 +0900

    10:53

--- 007.VueJS학습/02.기초연습/js/comp1.js
+++ 007.VueJS학습/02.기초연습/js/comp1.js
@@ -21,3 +21,23 @@ Vue.component("tit-comp", {
 
 // 뷰인스턴스 생성하기
 makeVue(".tit");
+
+// 2. 갤러리 리스트에 넣을 전역 컴포넌트 만들기
+Vue.component("list-comp",{
+    // (2-1) template 옵션 : 태그 구성
+    // v-bind:속성 디렉티브 변수를 사용할 수 있는 속성 재구성해 줌
+    template: `
+    <div>
+      <img v-bind:src="
+      './images/' + n + '.jpg'
+      " alt="의류아이템">
+      <aside>
+        <h2>aa</h2>
+        <h3>qq</h3>
+      </aside>
+    </div>
+    `,
+}); ////// component //////
+
+// 뷰인스턴스 생성하기
+makeVue(".grid");
