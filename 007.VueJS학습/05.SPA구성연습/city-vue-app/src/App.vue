f3f941ea60e742ac296d36daf990c70b17aa1c44
Sujin <9_ight@naver.com>
2024-08-09 12:20:11 +0900

    12:20

--- 007.VueJS학습/05.SPA구성연습/city-vue-app/src/App.vue
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/App.vue
@@ -1,17 +1,23 @@
+<!-- 1. 템플릿 구역 -->
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="app">
+    <TopArea />
   </div>
 </template>
 
+<!-- 2. 스트립트 구역 -->
 <script>
-export default {
-  name: 'App'
-}
+  import TopArea from './components/TopArea.vue';
+  
+  export default {
+    // 내보낼 컴포넌트 등록
+  components: { TopArea },
+    name: 'App'
+  }
 </script>
 
-<style>
+<!-- 3. 스타일 구역 : scoped 키워드 없으면 전역 -->
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- 007.VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
@@ -1,14 +1,25 @@
 // 맨 처음 읽어들이는 JS 파일 - main.js
 import Vue from 'vue'
-import App from './App'
+// 구성 Root Vue 파일
+import App from './App.vue'
+// 뷰JS 라우터 불러오기
 import router from './router'
+// 뷰엑스 스토어 JS 불러오기
+import store from './store'
 
+// 팁 안 나오게 하는 메서드
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  store, // 스토어 사용 등록!
+  router, // 라우터 사용 등록!
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  // 뷰 인스턴스 생성 직후 호출 코드 구역
+  created(){
+    // 데이터 초기화 메서드 호출
+    store.commit('initSet');
+  },
 })
