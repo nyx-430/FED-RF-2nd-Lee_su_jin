3b45aae999f24f0e6c35818e7ee89b276eaaf76e
Sujin <9_ight@naver.com>
2024-08-09 11:36:46 +0900

    11:36

--- 007.VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
+++ 007.VueJS학습/05.SPA구성연습/city-vue-app/src/main.js
@@ -1,5 +1,4 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 맨 처음 읽어들이는 JS 파일 - main.js
 import Vue from 'vue'
 import App from './App'
 import router from './router'
